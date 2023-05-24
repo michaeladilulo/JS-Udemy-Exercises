bae9d0862d6a04ad26ea96dc8c49f6be2b3369f7
michaeladilulo <michaeladilulo@gmail.com>
2023-05-23 23:48:26 -0600

    Udemy - Call function inside of function

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ function diceRoll() {
     let randomDiceRoll = Math.floor(Math.random() * 6) + 1
     console.log(randomDiceRoll)
 }
-
 diceRoll()
+
+// Roll dice multiple times
+function rollDice(numRolls) {
+    for(let i = 0; i < numRolls; i++) {
+        diceRoll();
+    }
+}
+rollDice(5); // will roll the dice 5 times
